4177f5a48b49f44010408fc86f630aa0bb9ad039
zhanel00 <zhanelgmstm@gmail.com>
2023-04-13 22:41:33 +0600

    created recipe-detail component

--- asui/src/app/app.module.ts
+++ asui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
