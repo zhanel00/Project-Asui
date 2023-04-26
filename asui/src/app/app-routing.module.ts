046b1bbfad6393e96ccd086d169af670d4f61277
OLDMONEY0 <aliar0703@gmail.com>
2023-04-26 16:34:01 +0600

    upd

--- asui/src/app/app-routing.module.ts
+++ asui/src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import {RecipeDetailsComponent} from "./recipe-details/recipe-details.component"
 import {AccountDetailsComponent} from "./account-details/account-details.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
+  { path:'', redirectTo:'/home', pathMatch:'full'},
+  { path: 'home', component: HomePageComponent},
   { path: 'recipe/:id', component: RecipeDetailsComponent},
-  {path: 'signIn', component:SignInComponent},
+  { path: 'signIn', component:SignInComponent},
   { path: 'signUp', component:SignUpComponent},
   { path: 'recipe/:id', component: RecipeDetailsComponent },
   { path: 'user/:id', component: AccountDetailsComponent }
